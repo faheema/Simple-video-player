1ee2e2b1a906e8a83bc6468c6a7c04f3940d8793
faheem.u <faheem.fu@gmail.com>
2017-12-23 16:26:56 +0530

    Simple Video Player

--- app/src/main/java/svp/com/fahm/simplevideoplayer/MainActivity.java
+++ app/src/main/java/svp/com/fahm/simplevideoplayer/MainActivity.java
@@ -1,7 +1,12 @@
 package svp.com.fahm.simplevideoplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import svp.com.fahm.simplevideoplayer.player.VideoPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnPlayVideo=findViewById(R.id.btnPlayVideo);
+
+        btnPlayVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, VideoPlayerActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/svp/com/fahm/simplevideoplayer/player/VideoPlayerActivity.java
@@ -0,0 +1,15 @@
+package svp.com.fahm.simplevideoplayer.player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import svp.com.fahm.simplevideoplayer.R;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}
