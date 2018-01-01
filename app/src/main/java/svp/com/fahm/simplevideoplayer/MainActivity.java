a6b4922f352af94d0f60a05704cbdebe3a473830
faheem.u <DFahmApps@gmail.com>
2018-01-02 01:27:23 +0530

    Update MainActivity.java

--- app/src/main/java/svp/com/fahm/simplevideoplayer/MainActivity.java
+++ app/src/main/java/svp/com/fahm/simplevideoplayer/MainActivity.java
@@ -7,9 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import svp.com.fahm.simplevideoplayer.player.VideoPlayerActivity;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
